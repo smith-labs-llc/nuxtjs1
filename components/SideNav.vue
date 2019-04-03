cb8b04781323bde7edf67dd8a8ac650e699c3bed
Michael Smith <finedesignz@users.noreply.github.com>
2019-04-02 19:46:44 -0700

    set dev environment

---
+++ components/SideNav.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list>
+    <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+      <v-list-tile-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title v-text="item.title" />
+      </v-list-tile-content>
+    </v-list-tile>
+  </v-list>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Admin',
+          to: '/admin'
+        }
+      ],
+      miniVariant: false
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <SideNav />
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import SideNav from '~/components/SideNav'
+export default {
+  components: {
+    SideNav
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false
+    }
+  }
+}
+</script>
